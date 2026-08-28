2074b0be4a71e57063f471121cddb31ecea5af55
chinthakuntasupraja_gmail.com <chinthakuntasupraja_gmail.com>
2026-08-28 22:13:38 +0000

    Add Databricks lakehouse implementation

---
+++ databricks/src/gold/dlt/explorations/sample_exploration.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
+# MAGIC %sql
+# MAGIC select *from spotify_cata.gold.dimuser
+# MAGIC
+# MAGIC select *
+
+# COMMAND ----------
+
